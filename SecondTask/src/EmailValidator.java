8a4d15dbffb5e43fa874884b3f297650b24bb21e
Ugniusst <ugnius.staniukynas@mif.stud.vu.lt>
2021-10-02 00:17:52 +0300

    Implement Email Validator

--- SecondTask/src/EmailValidator.java
+++ SecondTask/src/EmailValidator.java
@@ -1,17 +1,72 @@
+import java.util.ArrayList;
+
 public class EmailValidator {
+    private ArrayList<String> validTopLevelDomains = new ArrayList<>();
+
+    public EmailValidator(){
+        String[] defaultDomains = new String[]{"lt", "com", "ru", "org"};
+        for(String domain : defaultDomains) {
+            validTopLevelDomains.add(domain);
+        }
+    }
+    public EmailValidator(String[] validTopLevelDomains){
+        for(String domain : validTopLevelDomains) {
+            this.validTopLevelDomains.add(domain);
+        }
+    }
+
     public boolean checkEmailNotNull(String email) {
-        return false;
+        return (email != null);
     }
 
     public boolean checkIfEmailHasEta(String email) {
-        return false;
+        char[] emailSequence = email.toCharArray();
+        int etaCounter = 0;
+        for(char symbol : emailSequence) {
+            if(symbol == '@') {
+                etaCounter++;
+            }
+        }
+        if(etaCounter == 1) {
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 
     public boolean checkIfEmailDoesntContainInvalidSymbols(String email) {
-        return false;
+        char[] emailSequence = email.toCharArray();
+        char previousSymbol = ' ';
+        if(emailSequence[0] == '.' || emailSequence[0] == '@') {
+            return false;
+        }
+        for(char symbol : emailSequence) {
+            // 48-57 : numbers, 64-90: @ and A-Z letters,  97-122: a-z letters
+            if(!((symbol >= 48 && symbol <= 57) || (symbol >= 60 && symbol <= 90) || (symbol >= 97 && symbol <= 122))) {
+                if(symbol != '.' && symbol != '-' && symbol != '_') {
+                    return false;
+                }
+                else if(symbol == previousSymbol) {
+                    return false;
+                }
+                previousSymbol = symbol;
+            }
+        }
+        return true;
     }
 
     public boolean checkIfEmailDomainValid(String email) {
+        String domainPart = (email.split("@"))[1];
+        String[] domains = domainPart.split("\\.");
+        if(domains.length < 2) {
+            return false;
+        }
+        for(String validDomain : validTopLevelDomains) {
+            if (domains[domains.length - 1].equals(validDomain)) {
+                return true;
+            }
+        }
         return false;
     }
 }
