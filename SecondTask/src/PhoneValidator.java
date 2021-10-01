3fb61d322e4db1028307ba570c5cd27fb3ef518b
Ugniusst <ugnius.staniukynas@mif.stud.vu.lt>
2021-10-02 00:17:30 +0300

    Implement PhoneValidator

--- SecondTask/src/PhoneValidator.java
+++ SecondTask/src/PhoneValidator.java
@@ -1,21 +1,73 @@
+import java.util.ArrayList;
+
 public class PhoneValidator {
-    public boolean checkPhoneNotNull(String phoneNumber) {
-        return false;
-    }
 
-    public boolean checkPhoneOnlyNumbers(String phoneNumber) {
-        return false;
-    }
+    private ArrayList<ValidationPhoneRule> validationPhoneRules = new ArrayList<>();
 
+    public PhoneValidator(){
+        addValidationPhoneRule("LT", "+370", 12);
+        addValidationPhoneRule("FR", "+33", 13);
+    }
     public String changeThePrefix(String phoneNumber) {
-        return phoneNumber;
+        char[] phoneNumberSequence = phoneNumber.toCharArray();
+        char[] phoneNumberWithPrefix;
+        char[] prefix = new char[]{'+', '3', '7', '0'};
+        if(phoneNumberSequence[0] == '8') {
+            phoneNumberWithPrefix = new char[13];
+            for(int i=0; i<prefix.length; i++) {
+                phoneNumberWithPrefix[i] = prefix[i];
+            }
+            for(int i=1; i<phoneNumberSequence.length; i++) {
+                phoneNumberWithPrefix[3+i] = phoneNumberSequence[i];
+            }
+        }
+        else {
+            phoneNumberWithPrefix = phoneNumberSequence;
+        }
+        return String.valueOf(phoneNumberWithPrefix);
     }
 
     public boolean checkIfStartsWithCountryCode(String phoneNumber) {
+        char[] phoneNumberSequence = phoneNumber.toCharArray();
+        if(phoneNumberSequence[0] == '+') {
+            return true;
+        }
         return false;
     }
 
-    public boolean checkIfCountryIsValid(String countryCode) {
+    public boolean checkIfCountryIsValid(String countryCode, String phoneNumber) {
+        for(ValidationPhoneRule validationPhoneRule : validationPhoneRules) {
+            if(countryCode.equals(validationPhoneRule.countryCode)) {
+                char[] phoneNumberSequence = phoneNumber.toCharArray();
+                char[] prefix = validationPhoneRule.countryPhonePrefix.toCharArray();
+                for(int i=0; i<prefix.length; i++) {
+                    if(phoneNumberSequence[i]!=prefix[i]) {
+                        return false;
+                    }
+                }
+                if (phoneNumberSequence.length == validationPhoneRule.phoneNumberLength) {
+                    return true;
+                }
+                return false;
+            }
+        }
         return false;
+    }
+    public void addValidationPhoneRule(String countryCode, String countryPhonePrefix, int phoneNumberLength) {
+        validationPhoneRules.add(new ValidationPhoneRule(countryCode, countryPhonePrefix, phoneNumberLength));
+    }
+
+    public boolean checkPhoneNotNull(String phoneNumber) {
+        return (phoneNumber != null);
+    }
+
+    public boolean checkPhoneOnlyNumbers(String phoneNumber) {
+        char[] phoneNumberSequence = phoneNumber.toCharArray();
+        for(char symbol : phoneNumberSequence) {
+            if(symbol < 48 || symbol > 57) {
+                return false;
+            }
+        }
+        return true;
     }
 }
