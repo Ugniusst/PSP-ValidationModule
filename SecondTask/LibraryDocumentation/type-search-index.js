488b23d7baaa629e940da70fab0191aa240a5802
Ugniusst <71493945+Ugniusst@users.noreply.github.com>
2021-10-02 22:40:47 +0300

    Delete SecondTask/LibraryDocumentation directory

--- SecondTask/LibraryDocumentation/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"EmailValidator"},{"p":"<Unnamed>","l":"PasswordChecker"},{"p":"<Unnamed>","l":"PhoneValidator"},{"p":"<Unnamed>","l":"ValidationPhoneRule"}]
