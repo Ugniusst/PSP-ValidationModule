e35413a648f65fbeeb74f52dbc5cb71efa1a93b3
Ugniusst <ugnius.staniukynas@mif.stud.vu.lt>
2021-11-08 14:41:59 +0200

    Added User service with User validator
    User validator reduces dependency on validating module, so there would be no need to change a lot of ifs in service method

---
+++ ThirdTask/src/main/java/com/example/thirdtask/services/UserService.java
@@ -0,0 +1,57 @@
+package com.example.thirdtask.services;
+
+import ValidationLibrary.src.EmailValidator;
+import com.example.thirdtask.customExceptions.notValidateUserException;
+import com.example.thirdtask.entities.User;
+import com.example.thirdtask.repositories.UserRepository;
+import com.example.thirdtask.validators.UserValidator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    public User findById(int id) {
+        return userRepository.findById(id);
+    }
+
+    public Iterable<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public  User findByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    public User save(User user) throws notValidateUserException {
+
+        UserValidator userValidator = new UserValidator();
+
+        userValidator.validateEmail(user.getEmail());
+        userValidator.validatePassword(user.getPassword());
+        userValidator.validatePhoneNumber(user.getTelephoneNumber());
+
+        if(userRepository.findByEmail(user.getEmail()) == null) {
+            throw new notValidateUserException("Email is already used");
+        }
+
+        return userRepository.save(user);
+
+    }
+    public User update(int id, User user) throws notValidateUserException {
+        if(userRepository.findById(id) == null) {
+            throw new notValidateUserException("Updatable user is not found, consider creating a new usre");
+        }
+        user.setId(id);
+        return userRepository.save(user);
+    }
+    public void deleteById(int id) throws notValidateUserException {
+        if(userRepository.findById(id) == null) {
+            throw new notValidateUserException("The user with this id is not found");
+        }
+        userRepository.deleteById(id);
+    }
+}
